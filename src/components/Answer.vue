21da248a61127cf4ba424517a3301b8bba908daf
Akram Fawzi Mihoubi <mihhakram@gmail.com>
2024-08-19 01:18:29 +0100

    add style to answer

--- src/components/Answer.vue
+++ src/components/Answer.vue
@@ -1,10 +1,12 @@
 <template>
-    <label :for="id">
-        <input :disabled="disabled" type="radio" name="answer" :id="id" :value="value" v-model="model"/>
+    <label :for="id" :class="classes">
+        <input :id="id" name="answer" type="radio" :disabled="disabled" :value="value" v-model="model"/>
         {{ value }}
     </label>
 </template>
 <script setup>
+import { computed } from 'vue';
+
 const props = defineProps({
     id:String,
     value:String,
@@ -12,4 +14,19 @@ const props = defineProps({
     correctAnswer:String
 })
 const model= defineModel();
+const classes = computed(()=>({
+   right:props.disabled && props.value === props.correctAnswer,
+   wrong:props.disabled && props.value !== props.correctAnswer
+}))
 </script>
+
+<style>
+.right{
+    color: green;
+}
+
+.wrong{
+    color: red;
+}
+
+</style>
